fa257b946649c5579f1893841b3477ae8bd68380
Oscar Valdez <odvesims@gmail.com>
2021-08-01 15:45:02 -0400

    Organized code

--- index.js
+++ index.js
@@ -26,104 +26,74 @@ display_round_robin_header();
 
 const total_journeys = round_robin.divisions[0].group_teams.length - 1
 const matches_per_journey = round_robin.divisions[0].group_matches.length / total_journeys;
-
 var start_index = 0;
 var end_index = matches_per_journey;
-
 round_robin.divisions.forEach(division => {
-  
   display_round_robin_division_teams(division);
-
   display_round_robin_journeys(division, start_index, end_index, total_journeys, matches_per_journey);
 });
-
 display_tournament_header('UEFA CUP', '2021');
+display_round_robin_matches(round_robin, total_journeys, matches_per_journey, start_index, end_index);
 
 start_index = 0;
 end_index = matches_per_journey;
-
-display_round_robin_matches(round_robin, total_journeys, matches_per_journey, start_index, end_index);
-
 let round_robin_qualified_teams = get_round_robin_qualified_teams(round_robin.divisions, total_journeys);
-
 round_robin.arrange_teams(round_robin_qualified_teams.third_placers, total_journeys - 1);
-
 round_robin_qualified_teams.third_placers.splice(-2); //Trim the array so it only includes the top four 3rd placers.
 
 const play_offs = new FootballPlayOff(round_robin_qualified_teams, 1, scoring_match, ['']);
 play_offs.divide_teams();
 play_offs.set_up_matches();
 play_offs.play_matches();
-
-display_playoff_header();
-
 let round_of_sixteen_qualified_teams = play_offs.get_play_offs_qualified_teams(2);
-
+display_playoff_header();
 display_playoff_phase_header('Round of 16');
-
 display_playoff_phase_matches(play_offs.divisions[0].group_matches);
 
-display_playoff_phase_header('Quarter Finals');
-
 play_offs.divisions[0] = {name: 'Quartet Finals', group_teams: [], matched_teams: [], group_matches: []};
-
 round_of_sixteen_qualified_teams.forEach(qualified_teams => {
   play_offs.divisions[0].group_teams.push(qualified_teams.team_a);
   play_offs.divisions[0].group_teams.push(qualified_teams.team_b);
   play_offs.divisions[0].matched_teams.push({home: {team: qualified_teams.team_a, q: ''}, visitor: {team: qualified_teams.team_b, q: ''}});
 })
 play_offs.play_matches();
-
+display_playoff_phase_header('Quarter Finals');
 display_playoff_phase_matches(play_offs.divisions[0].group_matches);
 
-display_playoff_phase_header('Semi Finals');
-
 let quartet_finals_qualified_teams = play_offs.get_play_offs_qualified_teams(3);
-
 play_offs.divisions[0] = {name: 'Semi Finals', group_teams: [], matched_teams: [], group_matches: []};
-
 quartet_finals_qualified_teams.forEach(qualified_teams => {
   play_offs.divisions[0].group_teams.push(qualified_teams.team_a);
   play_offs.divisions[0].group_teams.push(qualified_teams.team_b);
   play_offs.divisions[0].matched_teams.push({home: {team: qualified_teams.team_a, q: ''}, visitor: {team: qualified_teams.team_b, q: ''}});
 })
 play_offs.play_matches();
-
+display_playoff_phase_header('Semi Finals');
 display_playoff_phase_matches(play_offs.divisions[0].group_matches);
 
-display_playoff_phase_header('Third and Fourth Place');
-
 var losers = [];
-
 let group_matches_temp = play_offs.divisions[0].group_matches;
 play_offs.divisions[0] = {name: 'Third and Fourth', group_teams: [], matched_teams: [], group_matches: []};
-
 group_matches_temp.forEach(group_match => {
   let match = group_match.matches[group_match.matches.length - 1];
   play_offs.divisions[0].group_teams.push(match.match_result.loser);
   losers.push(match.match_result.loser)
 });
 play_offs.divisions[0].matched_teams.push({home: {team: losers[0], q: ''}, visitor: {team: losers[1], q: ''}});
-
 play_offs.play_matches();
-
+display_playoff_phase_header('Third and Fourth Place');
 display_playoff_phase_matches(play_offs.divisions[0].group_matches);
 
-display_playoff_phase_header('FINALS');
-
 var winners = [];
-
 play_offs.divisions[0] = {name: 'Finals', group_teams: [], matched_teams: [], group_matches: []};
-
 group_matches_temp.forEach(group_match => {
   let match = group_match.matches[group_match.matches.length - 1];
   play_offs.divisions[0].group_teams.push(match.match_result.winner);
   winners.push(match.match_result.winner)
 });
 play_offs.divisions[0].matched_teams.push({home: {team: winners[0], q: ''}, visitor: {team: winners[1], q: ''}});
-
 play_offs.play_matches();
-
+display_playoff_phase_header('FINALS');
 display_playoff_phase_matches(play_offs.divisions[0].group_matches);
 
 let winner_name = play_offs.divisions[0].group_matches[play_offs.divisions[0].group_matches.length - 1].matches[play_offs.divisions[0].group_matches[play_offs.divisions[0].group_matches.length - 1].matches.length - 1].match_result.winner.name;
